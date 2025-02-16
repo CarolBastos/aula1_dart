c84ddf4398cddee658b8bb3fd1694f58da6d0bc3
Carol Bastos <bastoscarol292@gmail.com>
2025-02-16 10:41:07 -0400

    aula 4

--- bin/introducao_dart.dart
+++ bin/introducao_dart.dart
@@ -1,5 +1,13 @@
 import 'package:introducao_dart/introducao_dart.dart' as introducao_dart;
 
 void main(List<String> arguments) {
-  print('Hello world: ${introducao_dart.calculate()}!');
+  int variavel1 = 1;
+  int variavel2 = 2;
+
+  print('Resultado numeros inteiros: ${introducao_dart.multiplicacaoNumerosInteiros(variavel1,variavel2)}!');
+
+  double double1 = 2.2;
+  double double2 = 2.2;
+
+  print('Resultado números flutuantes: ${introducao_dart.mulplicacaoNumerosDecimais(double1,double2)}!');
 }
--- lib/introducao_dart.dart
+++ lib/introducao_dart.dart
@@ -1,3 +1,7 @@
-int calculate() {
-  return 6 * 7;
+int multiplicacaoNumerosInteiros(int numero1, int numero2) {
+  return numero1 * numero2;
+}
+
+double mulplicacaoNumerosDecimais(double numero1, double numero2) {
+  return numero1 * numero2;
 }
